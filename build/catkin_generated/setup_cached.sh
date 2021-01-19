9de413fa3dfdc664f75c5784b57407271be7cabc
Billy Ballico <will.ballico@gmail.com>
2021-01-20 02:59:14 +1100

    fixed differential to work correctly by connecting both bogies to the same rotational joint

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/billb/rover_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/billb/rover_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/billb/rover_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/billb/rover_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/billb/rover_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/billb/rover_ws/src:$ROS_PACKAGE_PATH"
